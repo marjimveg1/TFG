1f7d6588084463407f9f5fb93408618fe3208cb3
María Jiménez Vega <36474784+marjimveg1@users.noreply.github.com>
2020-02-09 20:38:31 +0100

    Revert "Modelos"
    This reverts commit 171b4925428e04bb6a25b99fdac6b74129f337c3.

--- Universidad/Universidad/Apps/Gestion/admin.py
+++ Universidad/Universidad/Apps/Gestion/admin.py
@@ -7,7 +7,7 @@ from django.contrib import admin
 
 
 from django.contrib import admin
-from Universidad.Apps.Gestion.models import Mama, Calendario, Fecha, Diario,Medicacion,Medida,Fotografia,Tension,Patada,Peso,Contraccion
+from Universidad.Apps.Gestion.models import Alumno
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
@@ -17,18 +17,7 @@ User=get_user_model()
 
 # Register your models here.
 #admin.site.register(User)
-admin.site.register(Mama)
-admin.site.register(Calendario)
-admin.site.register(Diario)
-admin.site.register(Fecha)
-admin.site.register(Medida)
-admin.site.register(Tension)
-admin.site.register(Medicacion)
-admin.site.register(Contraccion)
-admin.site.register(Fotografia)
-admin.site.register(Patada)
-admin.site.register(Peso)
-
+admin.site.register(Alumno)
 
 class UserAdmin(BaseUserAdmin):
 
--- Universidad/Universidad/Apps/Gestion/models.py
+++ Universidad/Universidad/Apps/Gestion/models.py
@@ -5,82 +5,18 @@ from django.db import models
 
 
 
-class Mama(models.Model):
-    nombre = models.CharField(max_length=25)
-    apellidos = models.CharField(max_length=40)
-    email = models.EmailField()
-    fechaNacimiento = models.DateField()
-    direccion =models.CharField(max_length=55)
-    fechaUltMens = models.DateField()
+class Alumno(models.Model):
+    Apellido = models.CharField(max_length=35)
+    Nombre = models.CharField(max_length=35)
+
+    def NombreCompleto(self):
+        cadena = "{0} {1}"
+
+        return cadena.format(self.Apellido, self.Nombre)
+
+
+
 
     def __str__(self):
-        return self.nombre + " " + self.apellidos
-
-
-class Calendario (models.Model):
-    mama = models.OneToOneField(Mama, null = False, default='DEFAULT VALUE')
-
-class Fecha(models.Model):
-    titulo = models.CharField(max_length=25)
-    momentoInicio = models.DateTimeField ()
-    momentoFin = models.DateTimeField ()
-    calendario = models.ForeignKey(Calendario, on_delete=models.CASCADE, null=False, blank=False, default='DEFAULT VALUE')
-
-    def __str__(self):
-        return self.titulo
-
-class Diario(models.Model):
-    mama = models.OneToOneField(Mama, null = False, default='DEFAULT VALUE')
-
-class Medida(models.Model):
-    fecha = models.DateField()
-    dBiparieta = models.IntegerField()
-    cAbdominal = models.IntegerField()
-    lFemur = models.IntegerField()
-    diario = models.ForeignKey(Diario, on_delete=models.CASCADE, null=False, blank=False, default='DEFAULT VALUE')
-
-
-class Fotografia(models.Model):
-    enlace = models.CharField(max_length=1000)
-    despription = models.CharField(max_length=1000, null=True, blank = True)
-    diario = models.ForeignKey(Diario, on_delete=models.CASCADE, null=False, blank=False, default='DEFAULT VALUE')
-
-class Patada(models.Model):
-    momento = models.DateTimeField ()
-    despription = models.CharField(max_length=1000, null=True, blank = True)
-    diario = models.ForeignKey(Diario, on_delete=models.CASCADE, null=False, blank=False, default='DEFAULT VALUE')
-
-class Tension(models.Model):
-    momento = models.DateTimeField ()
-    tSistolica = models.DecimalField(max_digits=4, decimal_places=2)
-    tDiastolica = models.DecimalField(max_digits=4, decimal_places=2)
-    diario = models.ForeignKey(Diario, on_delete=models.CASCADE, null=False, blank=False, default='DEFAULT VALUE')
-
-class Medicacion(models.Model):
-    medicamento = models.CharField(max_length=50)
-    fechaInicio = models.DateField()
-    fechaFin = models.DateField()
-    frecuencia = models.IntegerField()
-    diario = models.ForeignKey(Diario, on_delete=models.CASCADE, null=False, blank=False, default='DEFAULT VALUE')
-
-class Peso(models.Model):
-    tipo_choice = (
-        ('Madre', 'Madre'),
-        ('Bebe', 'Bebe')
-    )
-
-    fecha = models.DateField()
-    peso = models.DecimalField(max_digits=5, decimal_places=2)
-    tipo = models.CharField(max_length=100, choices=tipo_choice)
-    diario = models.ForeignKey(Diario, on_delete=models.CASCADE, null=False, blank=False, default='DEFAULT VALUE')
-
-class Contraccion(models.Model):
-    momento = models.DateTimeField ()
-    despription = models.CharField(max_length=1000, null=True)
-    diario = models.ForeignKey(Diario, on_delete=models.CASCADE, null=False, blank=False, default='DEFAULT VALUE')
-
-
-
-
-
-
+        return self.NombreCompleto()
+# Create your models here.
