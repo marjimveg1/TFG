a2d4ee119ec2496dcff6874bff441d2bd44ca092
marjimveg1 <maria-2696@hotmail.com>
2020-04-21 20:02:17 +0200

    asdfas

--- Universidad/Universidad/settings.py
+++ Universidad/Universidad/settings.py
@@ -82,6 +82,18 @@ WSGI_APPLICATION = 'Universidad.wsgi.application'
 #       'NAME': 'Universidad.db',
 #   }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'basedatostfg',
+        'USER': 'tfg',
+        'PASSWORD': '1233',
+        'HOST': 'localhost',
+        'PORT': ''
+    }
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
--- Universidad/Universidad/urls.py
+++ Universidad/Universidad/urls.py
@@ -20,7 +20,6 @@ from django.conf import settings
 from django.conf.urls import url, re_path
 from django.contrib.auth import views as auth_views
 from Universidad.Apps.Gestion.views import *
-from django.conf.urls.static import static
 
 
 urlpatterns = [
